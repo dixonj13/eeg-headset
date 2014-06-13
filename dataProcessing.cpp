7d6145cf722758fecdb326925afbe74314fde7f0
eeglabecu <eeglabecu@gmail.com>
2014-06-13 11:48:16 -0400

    Modified)

--- dataProcessing.cpp
+++ dataProcessing.cpp
@@ -15,6 +15,28 @@ using namespace std;
 const int SIZE_OF_FFT = 16;
 const int FOURIER_TYPE = 1;
 
+void fillBuffer(headset h, RDB Data, RawBQ RBQ)
+{
+	int numberSamples = h.get_num_samples();
+	int numberChannels = h.get_num_channels();
+
+	for(int i = h.get_current_index(); i < numberSamples; i++)
+	{
+		for(int y = 0; y < numberChannels; y++)
+		{
+			Data->channel_data_buffer[y][Data->dataUsed] = h.get_buffer_data(y,i);
+			if(isFull(Data))
+			{
+				add_raw_data_buffer(RBQ, Data);
+				//Deallocate Data
+				Data = new raw_data_buffer(h);
+			}
+			Data->dataUsed++;
+		}
+		h.set_current_index(i);
+	}
+}
+
 void write_fft_buffer(int NFFT, RDB Data, FILE* F)
 {
 	for(int i = 0; i < NFFT; i++)
@@ -35,36 +57,43 @@ void processRawData(RawBQ RBQ, headset h)
 	h.channel_CSV_write(F);
 	h.channel_CSV_write(C);
 
-	RDB rawData;
+	RDB rawData = NULL;
 	int Nx = SIZE_OF_FFT;
 	int NFFT = NFFTPowerTwoSamples(Nx);
 	double* imagineArray;
 
 	while(true) /*Time has not stopped, button is not pressed*/
 	{
-		//Writes raw data to file.
-		remove_raw_data_buffer(RBQ, rawData);
-		file_write_raw_data_buffer(F, rawData);
-
-		//Preforms FFT
-		for(int i = 0; i<rawData->numChannels; i++)
+		if(!isEmpty(RBQ))
 		{
-			imagineArray = createLargeArray(NFFT);
-			fillAndPad(imagineArray, rawData->channel_data_buffer[i], Nx, NFFT);
-			four1(imagineArray, NFFT, FOURIER_TYPE);
+			//Writes raw data to file
+			remove_raw_data_buffer(RBQ, rawData);
+			file_write_raw_data_buffer(F, rawData);
 
-			//Store imagineArray back into RDB
-			rawData->channel_data_buffer[i] = imagineArray;
+			//Preforms FFT
+			for(int i = 0; i<rawData->numChannels; i++)
+			{
+				imagineArray = createLargeArray(NFFT);
+				fillAndPad(imagineArray, rawData->channel_data_buffer[i], Nx, NFFT);
+				four1(imagineArray, NFFT, FOURIER_TYPE);
 
+				//Store imagineArray back into RDB
+				rawData->channel_data_buffer[i] = imagineArray;
+
+			}
+			//Write out FFT vaules Here
+			write_fft_buffer(NFFT, rawData, F);
+
+			//Delete rawData
 		}
-		//Write out FFT vaules Here
-		write_fft_buffer(NFFT, rawData, F);
-
-		//Delete rawData
 	}
 	fclose(F);
 	fclose(C);
 }
 
+int main(int argc, char** argv)
+{
+	return 0;
+}
 
 
